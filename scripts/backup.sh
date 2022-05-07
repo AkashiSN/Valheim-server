25108b76fd8a2ec038a421689762e3add23462fc
AkashiSN <btorntireinvynriy@gmail.com>
2022-05-07 22:34:58 +0900

    fix script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,11 +9,11 @@ DATETIME=$(date +"%Y%m%d%H%M%S")
 echo "Now: ${DATETIME}"
 
 BACKUP_DIR="/mnt/backup/valheim"
-DIRECTORY_BACKUP="${BACKUP_DIR}/valheim-${DATETIME}.tar.gz"
+DIRECTORY_BACKUP="${BACKUP_DIR}/valheim-${DATETIME}.tar.xz"
 
 MAX_BACKUP=30
 
-DIR_BACKUP_LIST=($(search_file ${BACKUP_DIR} '*.tar.gz'))
+DIR_BACKUP_LIST=($(search_file ${BACKUP_DIR} '*.tar.xz'))
 if [ ${#DIR_BACKUP_LIST[@]} -gt ${MAX_BACKUP} ]; then # 最大バックアップ数より多い場合古いバックアップを削除する
   for DIR_BACKUP in "${DIR_BACKUP_LIST[@]: 0:$((${#DIR_BACKUP_LIST[@]}-${MAX_BACKUP}))}"; do
     echo "Remove ${DIR_BACKUP}"
@@ -22,4 +22,4 @@ if [ ${#DIR_BACKUP_LIST[@]} -gt ${MAX_BACKUP} ]; then # 最大バックアップ
 fi
 
 echo "Backup valheim dir"
-tar zcf "${DIRECTORY_BACKUP}" -C /root/saves .
+tar Jcf "${DIRECTORY_BACKUP}" -C /root/saves .
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -11,12 +11,15 @@ DATETIME="$1"
 echo "Use: ${DATETIME}"
 
 BACKUP_DIR="/mnt/backup/valheim"
-DIRECTORY_BACKUP="${BACKUP_DIR}/valheim-${DATETIME}.tar.gz"
+DIRECTORY_BACKUP="${BACKUP_DIR}/valheim-${DATETIME}.tar.xz"
 
 if [ ! -e ${DIRECTORY_BACKUP} ];then
   echo "${DIRECTORY_BACKUP} is not exists."
   exit 1
 fi
+
+rm -rf /root/saves
+mkdir -p /root/saves
 
 echo "Restore valheim dir"
 tar xf "${DIRECTORY_BACKUP}" -C /root/saves .
